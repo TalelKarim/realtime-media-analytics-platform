6680ca3a78e65a9752a2dab8c52b1be8a3fd30d7
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-06-16 19:10:41 +0200

    infra(dynamodb): add realtime state tables

--- terraform/environments/dev/kinesis.tf
+++ terraform/environments/dev/kinesis.tf
@@ -6,6 +6,7 @@ module "kinesis" {
 
   kms_key_arn = module.kms.kinesis_key_arn
 
+
   shard_count            = var.kinesis_shard_count
   retention_period_hours = var.kinesis_retention_period_hours
   shard_level_metrics    = var.kinesis_shard_level_metrics
