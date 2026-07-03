322d8ca87416f13d525f9dda9d199bb313e81663
Talel Karim Chebbi <talelchebbi090@gmail.com>
2026-07-03 18:53:58 +0200

    Fix: typo input quicksight

--- terraform/environments/dev/quicksight.tf
+++ terraform/environments/dev/quicksight.tf
@@ -9,8 +9,8 @@ module "quicksight" {
   quicksight_principal_arn = var.quicksight_principal_arn
 
   glue_database_name   = module.glue_catalog.database_name
-  datalake_bucket_name = module.data_lake.bucket_name
-  s3_kms_key_arn       = module.kms.s3_kms_key_arn
+  datalake_bucket_name = module.s3_datalake.bucket_name
+  s3_kms_key_arn       = module.kms.s3_key_arn
 
   tags = local.tags
 }
