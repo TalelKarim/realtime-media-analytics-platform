a81d25f15af1ac41dada77f21ba8c6f8601de11c
Talel Karim Chebbi <talelchebbi090@gmail.com>
2026-07-28 14:55:42 +0200

    Fix(infra): duplicate vars sqs

--- terraform/modules/sqs/outputs.tf
+++ terraform/modules/sqs/outputs.tf
@@ -1,43 +1,8 @@
-output "broadcast_signal_queue_name" {
-  description = "Name of the broadcast signal FIFO queue."
-  value       = aws_sqs_queue.broadcast_signal.name
-}
-
-output "broadcast_signal_queue_url" {
-  description = "URL of the broadcast signal FIFO queue."
-  value       = aws_sqs_queue.broadcast_signal.url
-}
-
-output "broadcast_signal_queue_arn" {
-  description = "ARN of the broadcast signal FIFO queue."
-  value       = aws_sqs_queue.broadcast_signal.arn
-}
-
-output "broadcast_signal_dlq_name" {
-  description = "Name of the broadcast signal dead-letter FIFO queue."
-  value       = aws_sqs_queue.broadcast_signal_dlq.name
-}
-
-output "broadcast_signal_dlq_url" {
-  description = "URL of the broadcast signal dead-letter FIFO queue."
-  value       = aws_sqs_queue.broadcast_signal_dlq.url
-}
-
-output "broadcast_signal_dlq_arn" {
-  description = "ARN of the broadcast signal dead-letter FIFO queue."
-  value       = aws_sqs_queue.broadcast_signal_dlq.arn
-}
-
-
-
 # =============================================================================
 # Existing broadcast signal queue
 # =============================================================================
 
-output "broadcast_signal_queue_name" {
-  description = "Name of the broadcast signal FIFO queue."
-  value       = aws_sqs_queue.broadcast_signal.name
-}
+
 
 output "broadcast_signal_queue_url" {
   description = "URL of the broadcast signal FIFO queue."
