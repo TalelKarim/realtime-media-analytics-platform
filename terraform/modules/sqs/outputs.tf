b8ff48f90e1d6915e6649bf8e069bc2c8de6cdcf
Talel Karim Chebbi <talelchebbi090@gmail.com>
2026-07-28 15:03:35 +0200

    Fix(infra): Missing output sqs

--- terraform/modules/sqs/outputs.tf
+++ terraform/modules/sqs/outputs.tf
@@ -14,6 +14,11 @@ output "broadcast_signal_queue_arn" {
   value       = aws_sqs_queue.broadcast_signal.arn
 }
 
+output "broadcast_signal_queue_name" {
+  description = "Name of the broadcast signal FIFO queue."
+  value       = aws_sqs_queue.broadcast_signal.name
+}
+
 output "broadcast_signal_dlq_name" {
   description = "Name of the broadcast signal dead-letter FIFO queue."
   value       = aws_sqs_queue.broadcast_signal_dlq.name
